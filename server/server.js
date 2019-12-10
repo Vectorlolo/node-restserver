047bf5438373029152302420c5675f104a4ebd10
Victor <victorbri2112mail.com>
2019-12-10 18:52:42 -0400

    Npm carajox2 >:(

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.get('/usuario', function(req, res) {
+app.get('/', function(req, res) {
     res.json('get Usuario LOCAL!!!');
 });
 
